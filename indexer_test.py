456839db3d9702d51e2649b433f2625ff4c8c001
Marian Steinbach <marian@giantswarm.io>
2020-12-01 16:27:39 +0100

    Adapt test to no longer parse TOML

--- indexer_test.py
+++ indexer_test.py
@@ -13,18 +13,6 @@ categories: ["basics"]
 This is the YAML example's text
 """
 
-doc_with_toml_front_matter = """+++
-title = "The Giant Swarm App Catalog"
-description = "Overview of the Giant Swarm App Catalog, how it works and what to expect."
-date = "2019-02-11"
-weight = 90
-type = "page"
-categories = ["basics"]
-+++
-
-This is the TOML example's text
-"""
-
 doc_without_front_matter = """# Headline 1
 
 The _Giant Swarm App Catalog_ refers to a set of features and concepts that allow
@@ -35,11 +23,6 @@ from a single place; the Control Plane.
 
 
 class TestFrontMatter(unittest.TestCase):
-
-    def test_get_front_matter_toml(self):
-        data, text = get_front_matter(doc_with_toml_front_matter, "tomlpath")
-        self.assertEqual(data["title"], "The Giant Swarm App Catalog")
-        self.assertEqual(text, "This is the TOML example's text")
 
     def test_get_front_matter_yaml(self):
         data, text = get_front_matter(doc_with_yaml_front_matter, "yamlpath")
