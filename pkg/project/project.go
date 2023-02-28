d1a7cde718213b7072ee217d01abedd03ec8a79a
Marian Steinbach <marian@giantswarm.io>
2023-02-28 12:26:22 +0100

    Revert "Delete Go project file"
    This reverts commit 96938079d1cc7c40a15243cc8a16333981ba8b2e.

---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description        = "Indexes content for the search engine in docs.giantswarm.io"
+	gitSHA             = "n/a"
+	name        string = "docs-indexer"
+	source      string = "https://github.com/giantswarm/docs-indexer"
+	version            = "3.1.2-dev"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
