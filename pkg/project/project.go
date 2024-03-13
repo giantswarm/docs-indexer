a389fd0e96c5998071cb5f730fc20f91773eacc0
taylorbot <dev@giantswarm.io>
2024-03-13 12:20:46 +0000

    Release v3.4.2

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "docs-indexer"
 	source      string = "https://github.com/giantswarm/docs-indexer"
-	version            = "3.4.2-dev"
+	version            = "3.4.2"
 )
 
 func Description() string {
