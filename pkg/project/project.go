bc455440ddb30777912eb92b095bec334458532a
taylorbot <dev@giantswarm.io>
2025-09-17 07:21:22 +0000

    Bump version to 3.5.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "docs-indexer"
 	source      string = "https://github.com/giantswarm/docs-indexer"
-	version            = "3.5.0"
+	version            = "3.5.1-dev"
 )
 
 func Description() string {
