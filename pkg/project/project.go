27d4b4b3635dd9f17968ceeb0451973301ac2e92
taylorbot <dev@giantswarm.io>
2026-01-06 11:20:33 +0000

    Bump version to 4.1.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "docs-indexer"
 	source      string = "https://github.com/giantswarm/docs-indexer"
-	version            = "4.1.0"
+	version            = "4.1.1-dev"
 )
 
 func Description() string {
